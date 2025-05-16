3c38a473855550a5270c9a303a471988cc6f3372
karthik.rp <karthik.rp@elmeasure.com>
2025-05-16 14:53:01 +0530

    fetching data from endpoint and give dataFrame response to the UI user jinjaTemplates to give dashboard template

--- funds.py
+++ funds.py
@@ -1,8 +1,11 @@
+import traceback
 import aiohttp
 import os
-from fastapi import APIRouter, HTTPException, Query, Depends
+from fastapi import APIRouter, HTTPException,Request
 from dotenv import load_dotenv
-from auth import users_collection, get_current_user
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import pandas as pd
 
 router = APIRouter()
 load_dotenv()
@@ -14,29 +17,44 @@ BASE_URL = f"https://{RAPIDAPI_HOST}"
 url = f"{BASE_URL}/master"
 HEADERS = {"X-RapidAPI-Host": RAPIDAPI_HOST,
            "X-RapidAPI-Key": RAPIDAPI_KEY}
+templates = Jinja2Templates(directory="templates")
 
 
-@router.get("/funds")
-async def get_open_ended_funds(rta_agent_code: str = Query(..., description="e.g. CAMS, KARVY"),
-                               current_user: dict = Depends(get_current_user)):
+@router.get("/dashboard", response_class=HTMLResponse)
+async def dashboard(request: Request, rta_agent_code: str):
+    try:
+        df = await get_portfolio_df(rta_agent_code)
+        table_html = df.to_html(classes="dataframe", index=False)
 
-    params = {"RTA_Agent_Code": rta_agent_code}
-    email = current_user['email']
+        return templates.TemplateResponse("dashboard.html", {"request": request,
+                                                             "table": table_html,
+                                                             "rta_agent_code": rta_agent_code})
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url=url, headers=HEADERS, params=params) as response:
-            status = response.status
-            response_text = await response.text()
+    except Exception as e:
+        traceback.print_exc()
+        return str(e)
 
-            if status != 200:
-                raise HTTPException(status_code=500, detail=f"Failed to fetch fund data: {status} - {response_text}")
 
-            data = await response.json()
-            users_collection.update_one({"email": email}, {"$set": {"portfolio":data}}, upsert=True)
+async def get_portfolio_df(rta_agent_code: str) -> pd.DataFrame:
+    try:
+        params = {"RTA_Agent_Code": rta_agent_code}
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=url, headers=HEADERS, params=params) as response:
+                status = response.status
+                response_text = await response.text()
 
-    open_ended = [
-        fund for fund in data
-        if fund.get("Scheme_Type") == "Open Ended"
-    ]
+                if status != 200:
+                    raise HTTPException(status_code=500,
+                                        detail=f"Failed to fetch fund data: {status} - {response_text}")
 
-    return {"count": len(open_ended), "funds": open_ended}
+                data = await response.json()
+        form_data = []
+        for fund in data:
+            if fund['RTA_Agent_Code'] == rta_agent_code:
+                form_data.append({"RTA AGENT CODE": fund['RTA_Agent_Code'],
+                                  "AMC SCHEME CODE": fund['AMC_Scheme_Code'],
+                                  "MINIMUM PURCHASE AMOUNT": fund['Minimum_Purchase_Amount']})
+        return pd.DataFrame(form_data)
+    except Exception as e:
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail=str(e))
